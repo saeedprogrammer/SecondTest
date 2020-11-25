4ac74cfc10c4fd04f386fbe23c04af55c72d925d
saeedprogrammer <saeed.alasiry.h@hotmail.com>
2020-11-25 09:50:33 +0200

    second commit

--- SecondTest/ViewController.swift
+++ SecondTest/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("saeeed")
         let dd = "sssss"
+        let yy = ""
     }
 
 
